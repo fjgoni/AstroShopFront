6c413575cc7573854a0a5a177a3c8947d62b2b92
EdenCline <franciscojgoni@gmail.com>
2021-11-15 22:37:00 -0300

    Se agrego texto que se mueve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
